1ec561f9f52e5380aebd118745b7ede8d7bfe62d
Harsh Upadhayay <harshupadhayay906@gmail.com>
2021-10-28 12:01:14 +0530

    Abstraction

---
+++ EclipseWorkspace/TestDemo/src/Abstraction/Rectangle.java
@@ -0,0 +1,14 @@
+package Abstraction;
+
+public class Rectangle extends GraphicObject {
+	
+	void draw() {
+		System.out.println("Drawing a Rectangle");
+	}
+	
+	void resize() {
+		System.out.println("Resizing a Rectangle");
+	}
+	
+
+}
